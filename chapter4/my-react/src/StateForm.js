fa938e9eddb391a7cfa12d1d00fea4d349779d1b
Seri <2364138keigo@gmail.com>
2025-12-04 13:39:26 +0900

    教科書のサンプルコードに変更

--- chapter4/my-react/src/StateForm.js
+++ chapter4/my-react/src/StateForm.js
@@ -12,12 +12,39 @@ export const StateForm = () => {
       [e.target.name]: e.target.value, // e.target.valueは入力内容、valueは表示内容
     });
   };
+  const show = () => {
+    console.log(`こんにちは、${form.name}（${form.age}歳）さん！`);
+  };
   return (
-    <>
-      <p>name</p>
-      <input name="name" value={form.name} onChange={handleForm} />
-      <p>age</p>
-      <input name="age" value={form.age} onChange={handleForm} />
-    </>
+    <form>
+      <div>
+        <label htmlFor="name">名前：</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value={form.name}
+          onChange={handleForm}
+        />
+      </div>
+      <div>
+        <label htmlFor="age">年齢：</label>
+        <input
+          id="age"
+          name="age"
+          type="number"
+          value={form.age}
+          onChange={handleForm}
+        />
+      </div>
+      <div>
+        <button type="button" onClick={show}>
+          送信
+        </button>
+      </div>
+      <p>
+        こんにちは、{form.name}（{form.age}歳）さん！
+      </p>
+    </form>
   );
 };
